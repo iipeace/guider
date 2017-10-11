0f47f99eb169fd900ed24c8dd8d329b68552b083
Peace Lee <iipeace5@gmail.com>
2017-10-11 19:22:40 +0900

    Adding directory tree for new feature

---
+++ user/report.h
@@ -0,0 +1,20 @@
+/*
+ * Interfaces in report library
+ *
+ * Copyright (c) 2017 Peace Lee <iipeace5@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ */
+
+#ifndef __GUIDER_REPLIB_H__
+#define __GUIDER_REPLIB_H__
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#endif
