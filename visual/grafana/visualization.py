58300ee2d4ffbf9375586be0229418617fd67a31
pyotel <pyotel@gmail.com>
2018-08-30 17:44:30 +0900

    grafana output is being implemented

--- visual/grafana/visualization.py
+++ visual/grafana/visualization.py
@@ -8,3 +8,37 @@ pprint(data)
 print('-----------------------------------')
 
 print(data['mem']['kernel'])
+
+json_body = [
+    {
+        "measurement": "cpu_load_short",
+        "tags": {
+            "host": "server01",
+            "region": "us-west"
+        },
+        "time": "2009-11-10T23:00:00Z",
+        "fields": {
+            "value": 0.64
+        }
+    }
+]
+print('-----------------------------------')
+pprint(json_body)
+print('-----------------------------------')
+
+json_body_2 = list()
+
+json_dic = {
+    "measurement": "cpu_load_short",
+    "tags": {
+        "host": "server01",
+        "region": "us-west"
+    },
+    "time": "2009-11-10T23:00:00Z",
+    "fields": {
+        "value": 0.64
+    }
+}
+json_body_2.append(json_dic)
+json_body_2.append(json_dic)
+pprint(json_body_2)
