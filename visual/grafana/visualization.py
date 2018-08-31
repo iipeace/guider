7d79c9d34093e781074a2a43ac545f497af72f23
pyotel <pyotel@gmail.com>
2018-08-31 10:42:36 +0900

    json parsing and inserting influxDB

--- visual/grafana/visualization.py
+++ visual/grafana/visualization.py
@@ -1,44 +1,36 @@
 import json
 from pprint import pprint
+from influxdb import InfluxDBClient
+
+json_dic_tags = {
+        "host": "db_host_server",
+        "region": "aws_korea"
+    }
+
+json_body_list = list()
+
 
 with open('../sample_output/guider.report.sample') as data_file:
-    data = json.load(data_file)
+    guider_data = json.load(data_file)
 
-pprint(data)
-print('-----------------------------------')
+for super_k in guider_data.keys() :
+    json_dic = dict()
+    json_dic["tags"] = json_dic_tags
+    print(super_k)
+    json_dic["measurement"] = super_k
+    json_dic["fields"] = dict()
+    for sub_k in guider_data[super_k] :
+        print(super_k + '_' + sub_k)
+        json_dic["fields"][sub_k] = guider_data[super_k][sub_k]
+    if len(json_dic["fields"]) > 0 :
+        json_body_list.append(json_dic)
 
-print(data['mem']['kernel'])
+print("-----------------------------------------")
+pprint(json_body_list)
+print("-----------------------------------------")
 
-json_body = [
-    {
-        "measurement": "cpu_load_short",
-        "tags": {
-            "host": "server01",
-            "region": "us-west"
-        },
-        "time": "2009-11-10T23:00:00Z",
-        "fields": {
-            "value": 0.64
-        }
-    }
-]
-print('-----------------------------------')
-pprint(json_body)
-print('-----------------------------------')
 
-json_body_2 = list()
-
-json_dic = {
-    "measurement": "cpu_load_short",
-    "tags": {
-        "host": "server01",
-        "region": "us-west"
-    },
-    "time": "2009-11-10T23:00:00Z",
-    "fields": {
-        "value": 0.64
-    }
-}
-json_body_2.append(json_dic)
-json_body_2.append(json_dic)
-pprint(json_body_2)
+client = InfluxDBClient('13.124.47.121', 8086, 'root', 'root', 'guider_data')
+client.write_points(json_body_list)
+result = client.query('select value from mem;')
+print("Result: {0}".format(result))
