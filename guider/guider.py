41b81b5b4e2a63084878723e2f2ceb7a26f2b8d3
Peace Lee <iipeace5@gmail.com>
2018-10-04 23:28:26 +0900

    Add float support in convertUnit2Size()

--- guider/guider.py
+++ guider/guider.py
@@ -8080,21 +8080,21 @@ class SystemManager(object):
 
         try:
             if value.endswith('K'):
-                return long(value[:-1]) * sizeKB
+                return long(float(value[:-1])) * sizeKB
             if value.endswith('KB'):
-                return long(value[:-2]) * sizeKB
+                return long(float(value[:-2])) * sizeKB
             if value.endswith('M'):
-                return long(value[:-1]) * sizeMB
+                return long(float(value[:-1])) * sizeMB
             if value.endswith('MB'):
-                return long(value[:-2]) * sizeMB
+                return long(float(value[:-2])) * sizeMB
             if value.endswith('G'):
-                return long(value[:-1]) * sizeGB
+                return long(float(value[:-1])) * sizeGB
             if value.endswith('GB'):
-                return long(value[:-2]) * sizeGB
+                return long(float(value[:-2])) * sizeGB
             if value.endswith('T'):
-                return long(value[:-1]) * sizeTB
+                return long(float(value[:-1])) * sizeTB
             if value.endswith('TB'):
-                return long(value[:-2]) * sizeTB
+                return long(float(value[:-2])) * sizeTB
 
             raise Exception()
         except:
