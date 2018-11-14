5bcda1df5286e97cddd5cba0dc44ede535951c6f
Peace Lee <iipeace5@gmail.com>
2018-11-14 16:07:26 +0900

    Update setup.py to support python -m option

---
+++ guider/__main__.py
@@ -0,0 +1,4 @@
+import sys
+from guider import guider
+
+guider.main(sys.argv)
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import site, os
+import site, os, sys
 from distutils.core import setup
 
 setup(
@@ -12,54 +12,62 @@ setup(
     download_url = 'https://github.com/iipeace/guider/archive/master.zip',
     keywords = ['guider', 'linux', 'analyzer', 'performance', 'profile', 'trace', 'kernel'],
     packages = ['guider'],
-    data_files=[('/usr/sbin', ['guider/guider'])],
-    classifiers = [],
+    scripts = ['guider/guider'],
+    classifiers = [
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Operating System :: Android',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: System :: Filesystems',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Operating System Kernels',
+        ],
 )
 
+# make guider directory #
 try:
-    os.mkdir('/usr/share/guider')
+    desPath = '%s/share/guider' % sys.prefix
+    pycDesPath = '%s/guider.pyc' % desPath
+    os.mkdir(desPath)
 except:
     pass
 
+# search package path #
 for path in site.getsitepackages():
-    pycPath = '/usr/share/guider/guider.pyc'
-    pycOrig = '%s/guider/guider.pyc' % path
+    # check path #
+    if os.path.isdir('%s/guider' % path) is False:
+        continue
 
-    cachedPycDir = '%s/guider/__pycache__/' % path
-    if os.path.isdir(cachedPycDir):
-        for cache in os.listdir(cachedPycDir):
-            os.rename('%s%s' % (cachedPycDir, cache), pycOrig)
+    # set pyc source path #
+    pycSrcPath = '%s/guider/guider.pyc' % path
 
     try:
-        pycOrig = '%s/guider/guider.pyc' % path
+        # prepare source pyc file #
+        pycTmpPath = '%s/guider/__pycache__/' % path
+        if os.path.isdir(pycTmpPath):
+            for cache in os.listdir(pycTmpPath):
+                os.rename('%s%s' % (pycTmpPath, cache), pycSrcPath)
 
-        if os.path.isfile(pycOrig) is False:
+        # check pyc source file #
+        if os.path.isfile(pycSrcPath) is False:
             continue
 
-        if os.path.isfile(pycPath):
-            os.unlink(pycPath)
+        # remove old pyc file #
+        if os.path.isfile(pycDesPath):
+            os.unlink(pycDesPath)
 
-        cachedPycDir = '%s/guider/__pycache__/' % path
-        if os.path.isdir(cachedPycDir):
-            for cache in os.listdir(cachedPycDir):
-                os.rename('%s%s' % (cachedPycDir, cache), pycOrig)
-
-        os.symlink(pycOrig, pycPath)
-    except:
-        pass
-    try:
-        shPath = '/usr/sbin/guider'
-
-        if os.isfile(shPath):
-            os.remove(shPath)
-
-        os.symlink('%s/guider/usr/sbin/guider' % path, shPath)
+        # create pyc destination path #
+        os.symlink(pycSrcPath, pycDesPath)
     except:
         pass
 
 '''
-upload command
+pypi upload command
     # python setup.py sdist upload -r pypitest
     # python setup.py sdist upload -r https://upload.pypi.org/legacy/
 '''
-
