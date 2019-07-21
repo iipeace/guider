df36c0bf9798f14f00d81cf1e9bd28d2f4920de1
kdragonL <stftstft@naver.com>
2019-07-22 00:29:22 +0900

    agent/__init__.py modified for IP input fron web

--- agent/__init__.py
+++ agent/__init__.py
@@ -3,17 +3,17 @@ $ cd guider
 $ export FLASK_APP=agent
 $ flask run
 '''
-
 import os
 import sys
 import json
+import re
 
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, send, emit
 
 class RequestManager(object):
-    requests = {}
 
+    requests = {}
     def __new__(self):
         if not hasattr(self, 'instance'):
             self.instance = super(RequestManager, self).__new__(self)
@@ -51,58 +51,62 @@ app = CustomFlask(__name__, template_folder='./templates')
 # app.config['SECRET_KEY'] = 'XXXX'
 socketio = SocketIO(app)
 
-# add guider path here
+#add guider path here
 curDir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, '%s/../guider' % curDir)
 
 from guider import NetworkManager
-
-NetworkManager.prepareServerConn(None, None)
-
-# get connection with server #
-conn = NetworkManager.getServerConn()
-if not conn:
-    print('\nFail to get connection with server')
-    sys.exit(0)
-
-# request command #
-pipe = NetworkManager.getCmdPipe(conn, 'GUIDER top -a -J')
-if not pipe:
-    print('\nFail to get command pipe')
-    sys.exit(0)
-
 @app.route('/')
 def index():
-    return render_template('index_vue.html', server_addr=SERVER_ADDR)
+    return render_template('index.html', server_addr=SERVER_ADDR)
 
 @socketio.on('connect')
 def connected():
-    print("This is real-connect message") # this works naturally
+    print("Connected") 
 
-@socketio.on('disconnect') # Not Custom
+@socketio.on('disconnect')
 def disconnected():
     RequestManager.clear_request()
-#    print("Client is disconnected")
-
-@socketio.on('custom_connect') # this is custom one
-def custom_connect(msg):
-#    print("This Customed-connected_event!")
-    emit('server_response', {'data': msg})
 
 @socketio.on('request_start')
-def request_start(timestamp, msg):
+def request_start(timestamp, targetAddr):
+     
+    NetworkManager.prepareServerConn(None, targetAddr)
+    # get connection with server #
+    conn = NetworkManager.getServerConn()
+    if not conn:
+        print('\nFail to get connection with server')
+        sys.exit(0)
+
+    # request command #
+    pipe = NetworkManager.getCmdPipe(conn, 'GUIDER top -a -J')
+    if not pipe:
+        print('\nFail to get command pipe')
+        sys.exit(0)
+
+    print('Requested ----- ')
+    msg = {}
     msg['timestamp'] = timestamp
     RequestManager.add_request(timestamp)
     is_connected = RequestManager.get_request(timestamp)
+    cntGetData = -1
     while (is_connected==True):
         str_pipe = pipe.getData() # str type with json contents
-        length_pipe = len(str_pipe)
-        msg['length_pipe'] = str(length_pipe)
-        msg['str_pipe'] = str_pipe
-        print('Emit length : ', msg['length_pipe'])
-        emit('server_response', msg)
+        cntGetData = cntGetData + 1
+        try: # to catch out json parse error
+            json_pipe = json.loads(str_pipe)
+            msg['cpu_pipe'] = json.dumps(json_pipe["cpu"])
+            msg['mem_pipe'] = json.dumps(json_pipe["mem"])
+            msg['proc_pipe'] = json.dumps(json_pipe["process"])
+            length_pipe = len(str_pipe)
+            msg['length_pipe'] = str(length_pipe)
+            emit('server_response', msg)
+        except:
+            print("[" + str(cntGetData) + "]----------------Json parsing error----------------")
         is_connected = RequestManager.get_request(timestamp)
-    #connected(msg)
+        # time.sleep should not be used for its blocking thread or something.
+        # (related articles are found over stackoverflow or somewhere else)
+
 
 @socketio.on('request_stop')
 def request_stop(target_timestamp):
