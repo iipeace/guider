bf45611274d8e78a84235f1f39096d78c7b4dc48
kdragonL <32946448+kdragonL@users.noreply.github.com>
2019-08-19 06:52:01 +0900

    front-side source files are included (#50)
    * Agent files are updated. 1> input target ip/port(agent/__init__.py) 2> vue complied js, html files include
    
    * __init__.py modified for JSON type checking and so on.
    
    * Useless files are deleted
    
    * The front-side source files are included. These work upon the vue-cli development environment

---
+++ agent/front_vuejs/src/PieChart.vue
@@ -0,0 +1,22 @@
+<script>
+import { Pie, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+export default {
+  extends: Pie,
+  mixins: [reactiveProp],
+  props: ["options"],
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
+
+<style lang="scss">
+canvas {
+
+
+}
+
+</style>
