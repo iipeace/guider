a2ce50c95d6ec10ff9455a001b971f7d0bff6809
yangsso <didthglaso@gmail.com>
2019-10-13 12:13:53 +0900

    wip code comment out & chart option add

--- agent/front-vue/src/pages/Dashboard.vue
+++ agent/front-vue/src/pages/Dashboard.vue
@@ -1,33 +1,31 @@
 <template>
   <div>
     <b-row>
-      <b-col lg="12">
+      <b-col lg="6">
         <b-card no-body>
           <b-card-header>
             <h4>CPU</h4>
           </b-card-header>
           <b-card-body>
             <vue-apex-charts
-              width="100%"
               height="300"
               type="area"
-              :options="chartOptions"
+              :options="stackChartOptions"
               :series="dataSet.cpuSeries"
             />
           </b-card-body>
         </b-card>
       </b-col>
-      <b-col lg="12">
+      <b-col lg="6">
         <b-card no-body>
           <b-card-header>
             <h4>MEMORY</h4>
           </b-card-header>
           <b-card-body>
             <vue-apex-charts
-              width="100%"
               height="300"
               type="area"
-              :options="chartOptions"
+              :options="stackChartOptions"
               :series="dataSet.memorySeries"
             />
           </b-card-body>
@@ -43,7 +41,7 @@
               width="100%"
               height="300"
               type="line"
-              :options="chartOptions"
+              :options="lineChartOptions"
               :series="dataSet.networkSeries"
             />
           </b-card-body>
@@ -64,9 +62,33 @@ export default {
   data: function() {
     return {
       dataSet: new GuiderDataSet(),
-      chartOptions: {
+      lineChartOptions: {
         xaxis: {
           name: "timestamp"
+        },
+        dataLabels: {
+          enabled: false
+        }
+      },
+      stackChartOptions: {
+        chart: {
+          stacked: true
+        },
+        stroke: {
+          curve: "smooth"
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            opacityFrom: 0.6,
+            opacityTo: 0.8
+          }
+        },
+        xaxis: {
+          name: "timestamp"
+        },
+        dataLabels: {
+          enabled: false
         }
       }
     };
