a307a2e6c2af7ea0d6305c0f4c27549c507c5582
agrajak <korbots@gmail.com>
2019-10-14 22:58:38 +0900

    Solve flake8 lint issues in controllers.py

--- agent/monitoring/controllers.py
+++ agent/monitoring/controllers.py
@@ -1,11 +1,7 @@
 from flask import make_response, render_template, request, jsonify
 from flask_restful import Resource
-<<<<<<< HEAD
 from datetime import datetime
 from monitoring.models import Data, spread_data, deserialize_data
-=======
-from monitoring.models import Data
->>>>>>> upstream/master
 
 
 class Main(Resource):
