e8813ca1dac37e5e6161c21a2672ab8ba5a1cc19
Suhyun Jeon <korbots@gmail.com>
2019-10-14 23:14:15 +0900

    Implement Dataset API(Fetch datas from MongoDB) (#139)
    * Add Storage Model in mornitoring/models.py
    Add Dataset REST API Endpoint (/dataset)
    
    * Add error handling for MongoDB
    
    * Add Serialize/Deserialize methods in models.py
    
    * Return cherrypicked values and status information in Dataset
    
    * Add typecasting for querystring 'num' in controllers.py
    
    * Solve flake8 lint issues in controllers.py

--- agent/monitoring/controllers.py
+++ agent/monitoring/controllers.py
@@ -1,6 +1,7 @@
 from flask import make_response, render_template, request, jsonify
 from flask_restful import Resource
-from monitoring.models import Data
+from datetime import datetime
+from monitoring.models import Data, spread_data, deserialize_data
 
 
 class Main(Resource):
@@ -17,25 +18,65 @@ class Main(Resource):
 
 
 class Dataset(Resource):
-
     def __init__(self):
         pass
 
     def get(self):
-        # args = request.args
-        # start = args.get('start', 0)
-        # end = args.get('end', int(time()))
-        # TODO: validate timestamp
-        # num = args.get('num', 20)
+        args = request.args
+        start = args.get('start', None)
+        end = args.get('end', None)
+        num = args.get('num', 20)
+        from_utc = 9 * 60 * 60
+        try:
+            if start is None:
+                start = (Data.objects.order_by('+timestamp').limit(1))
+                start = int(datetime.timestamp(start[0]['timestamp'])) + \
+                    from_utc
+            else:
+                start = int(start)
+            if end is None:
+                end = (Data.objects.order_by('-timestamp').limit(1))
+                end = int(datetime.timestamp(end[0]['timestamp'])) + from_utc
+            else:
+                end = int(end)
+            num = int(num)
+        except Exception as e:
+            print('Failed to parse start, end ', e)
+            return jsonify(dict(status="failed", data=[]))
+        datas = list(Data.objects().order_by('+timestamp').all())
+        print(len(datas), start, end)
+        # TODO: Quering Database in range, Not processing everything.
+        # TODO: only fetch data in a certain mac_address.
+        result = []
+        idx = 0
+        for c in range(num):
+            period = int((end-start)/num)
+            timestamp_bound = start+period*(c+1)
+            sum = {}
+            cnt = 0
+            while idx < len(datas):
+                timestamp = int(datetime.timestamp(datas[idx].timestamp)) + \
+                            from_utc
+                if timestamp > timestamp_bound:
+                    break
+                if (timestamp < start) or (timestamp > end):
+                    idx += 1
+                    continue
+                spread = spread_data(datas[idx])
+                for k in spread.keys():
+                    if k in sum:
+                        sum[k] += spread[k]
+                    else:
+                        sum[k] = 0
+                idx += 1
+                cnt += 1
 
-        # last = Data.objects().order_by('-timestamp').limit(1)
-        # first = Data.objects().order_by('+timestamp').limit(1)
+            result.append(dict(
+                timestamp=timestamp_bound,
+                data=deserialize_data(sum, cnt)
+            ))
 
-        # print(first['timestamp'], last['timestamp'])
-        # print('start:timestamp = ', data[0]['timestamp'])
-        # print('end:timestamp = ', data[-1]['timestamp'])
-        # return jsonify(data)
-        return jsonify(Data.objects())
+        return jsonify(dict(status="ok", data=result))
 
 
 class Slack(Resource):
--- agent/monitoring/models.py
+++ agent/monitoring/models.py
@@ -5,30 +5,30 @@ db = MongoEngine()
 
 
 class CPU(db.EmbeddedDocument):
-    kernel = db.IntField()
-    user = db.IntField()
-    irq = db.IntField()
-    nrCore = db.IntField()
-    total = db.IntField()
+    kernel = db.IntField(default=0)
+    user = db.IntField(default=0)
+    irq = db.IntField(default=0)
+    nrCore = db.IntField(default=0)
+    total = db.IntField(default=0)
 
 
 class Memory(db.EmbeddedDocument):
-    kernel = db.IntField()
-    cache = db.IntField()
-    free = db.IntField()
-    anon = db.IntField()
-    total = db.IntField()
+    kernel = db.IntField(default=0)
+    cache = db.IntField(default=0)
+    free = db.IntField(default=0)
+    anon = db.IntField(default=0)
+    total = db.IntField(default=0)
 
 
 class Storage(db.EmbeddedDocument):
-    free = db.IntField()
-    usage = db.IntField()
-    total = db.IntField()
+    free = db.IntField(default=0)
+    usage = db.IntField(default=0)
+    total = db.IntField(default=0)
 
 
 class Network(db.EmbeddedDocument):
-    inbound = db.IntField()
-    outbound = db.IntField()
+    inbound = db.IntField(default=0)
+    outbound = db.IntField(default=0)
 
 
 class Data(db.Document):
@@ -38,3 +38,80 @@ class Data(db.Document):
     memory = db.EmbeddedDocumentField(Memory)
     storage = db.EmbeddedDocumentField(Storage)
     network = db.EmbeddedDocumentField(Network)
+
+
+def spread_data(data):
+    result = dict()
+    result.update(spread_cpu(data.cpu))
+    result.update(spread_storage(data.storage))
+    result.update(spread_memory(data.memory))
+    result.update(spread_network(data.network))
+    return result
+
+
+def spread_cpu(cpu):
+    return dict(cpu_kernel=cpu.kernel, cpu_user=cpu.user,
+                cpu_irq=cpu.irq, cpu_nrCore=cpu.nrCore,
+                cpu_total=cpu.total)
+
+
+def spread_storage(storage):
+    if storage:
+        return dict(
+            storage_free=storage.free,
+            storage_usage=storage.usage,
+            storage_total=storage.total
+        )
+    else:
+        return dict(
+            storage_free=0,
+            storage_usage=0,
+            storage_total=0
+        )
+
+
+def spread_memory(memory):
+    return dict(
+        memory_kernel=memory.kernel,
+        memory_cache=memory.cache,
+        memory_free=memory.free,
+        memory_anon=memory.anon,
+        memory_total=memory.total
+    )
+
+
+def spread_network(network):
+    return dict(
+        network_inbound=network.inbound,
+        network_outbound=network.outbound
+    )
+
+
+def deserialize_data(data, cnt):
+    if cnt == 0:
+        cnt = 1
+    return {
+        "cpu": {
+            "kernel": data.get('cpu_kernel', 0)/cnt,
+            "user": data.get('cpu_user', 0)/cnt,
+            "irq": data.get('cpu_irq', 0)/cnt,
+            "nrCore": data.get('cpu_nrCore', 0)/cnt,
+            "total": data.get('cpu_total', 0)/cnt
+        },
+        "storage": {
+            "free": data.get('storage_free', 0)/cnt,
+            "usage": data.get('storage_usage', 0)/cnt,
+            "total": data.get('storage_total', 0)/cnt
+        },
+        "memory": {
+            "kernel": data.get('memory_kernel', 0)/cnt,
+            "cache": data.get('memory_cache', 0)/cnt,
+            "free": data.get('memory_free', 0)/cnt,
+            "anon": data.get('memory_anon', 0)/cnt,
+            "total": data.get('memory_total', 0)/cnt
+        },
+        "network": {
+            "inbound": data.get('network_inbound', 0)/cnt,
+            "outbound": data.get('network_inbound', 0)/cnt
+        }
+    }
