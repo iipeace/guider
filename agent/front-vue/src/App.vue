88fa94191195528969b710be339d81f63a2a6995
woojin.kang <woojin.kang@linecorp.com>
2019-10-15 22:26:01 +0900

    Refactor layout

--- agent/front-vue/src/App.vue
+++ agent/front-vue/src/App.vue
@@ -1,42 +1,24 @@
 <template>
   <div>
-    <router-view :key="$route.fullPath"></router-view>
+    <top-navbar></top-navbar>
+    <side-nav-bar></side-nav-bar>
+    <div class="main-panel">
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TopNavbar from './layout/TopNavbar';
+import SideNavBar from './layout/SideNavBar';
+
 export default {
-  data() {
-    return {
-      colorCodes: [
-        "#DC143C",
-        "#008000",
-        "#000080",
-        "#b07025",
-        "#FFFF00",
-        "#BFFF00",
-        "#80FF00",
-        "#40FF00",
-        "#00FF00",
-        "#00FF40",
-        "#00FF80",
-        "#00FFBF",
-        "#00FFFF",
-        "#00BFFF",
-        "#0080FF",
-        "#0040FF"
-      ]
-    };
+  components: {
+    SideNavBar,
+    TopNavbar
   },
-  methods: {
-    toggleNavOpen() {
-      let root = document.getElementsByTagName("html")[0];
-      root.classList.toggle("nav-open");
-    }
-  },
-  mounted: function() {
-    this.$watch("$sidebar.showSidebar", this.toggleNavOpen);
-  }
 };
 </script>
 
---
+++ agent/front-vue/src/layout/SideNavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <side-bar>
+    <template slot="links">
+      <sidebar-link
+        to="/dashboard"
+        name="dashboard"
+        icon="tim-icons icon-chart-pie-36"
+      />
+      <sidebar-link
+        to="/command"
+        name="command"
+        icon="tim-icons icon-chart-pie-36"
+      />
+      <sidebar-link
+        to="/background"
+        name="background"
+        icon="tim-icons icon-chart-pie-36"
+      />
+    </template>
+  </side-bar>
+</template>
+
+<script>
+  export default {
+    name: "SideNavBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
