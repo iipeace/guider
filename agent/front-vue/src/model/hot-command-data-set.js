d2dedadbaa575e8e79c1b4e1c85b16a12eceda77
woojin.kang <woojin.kang@linecorp.com>
2019-10-16 22:06:43 +0900

    Add command helper and sample commands #128

--- agent/front-vue/src/model/hot-command-data-set.js
+++ agent/front-vue/src/model/hot-command-data-set.js
@@ -22,37 +22,224 @@ export class Command {
 
 export const HotCommandDataSet = [
   new Command("top", [
-    new Option("e", true, "description"),
-    new Option("d", true, ""),
-    new Option("o", true, ""),
-    new Option("u", false, ""),
-    new Option("W", false, ""),
-    new Option("b", true, ""),
-    new Option("T", true, ""),
-    new Option("j", true, ""),
-    new Option("w", true, ""),
-    new Option("x", true, ""),
-    new Option("X", true, ""),
-    new Option("N", true, ""),
-    new Option("S", true, ""),
-    new Option("P", false, ""),
-    new Option("I", true, ""),
-    new Option("m", true),
-    new Option("a", false),
-    new Option("g", true),
-    new Option("i", true),
-    new Option("R", true),
-    new Option("Q", false),
-    new Option("E", true),
-    new Option("H", true),
-    new Option("k", true),
-    new Option("z", true),
-    new Option("Y", true),
-    new Option("v", false)
+    new Option("e", true, "enable options\n" +
+                          "                a:affinity | b:block | c:cpu | C:cgroup\n" +
+                          "                d:disk | D:DLT | e:encode | E:Elastic\n" +
+                          "                f:float | F:wfc | h:sigHandler | i:irq\n" +
+                          "                L:cmdline | m:memory | n:net | o:oomScore\n" +
+                          "                p:pipe | P:perf | r:report | R:fileReport\n" +
+                          "                s:stack | S:pss | t:thread | u:uss\n" +
+                          "                w:wss | W:wchan"),
+    new Option("d", true, "disable options\n" +
+                          "                a:memAvailable | A:cpuAverage\n" +
+                          "                c:cpu | e:encode | G:gpu | L:log\n" +
+                          "                p:print | t:truncate | T:task"),
+    new Option("o", true, "save output data"),
+    new Option("u", false, "run in the background"),
+    new Option("W", false, "wait for signal"),
+    new Option("b", true, "set buffer size"),
+    new Option("T", true, "set font path"),
+    new Option("j", true, "set report path"),
+    new Option("w", true, "set additional command"),
+    new Option("x", true, "set local address"),
+    new Option("X", true, "set request address"),
+    new Option("N", true, "set report address"),
+    new Option("S", true, "sort by key"),
+    new Option("P", false, "group threads in same process"),
+    new Option("I", true, "set input path"),
+    new Option("m", true, "set terminal size"),
+    new Option("a", false, "show all stats and events"),
+    new Option("g", true, "set filter"),
+    new Option("i", true, "set interval"),
+    new Option("R", true, "set repeat count"),
+    new Option("Q", false, "print all rows in a stream"),
+    new Option("E", true, "set cache dir path"),
+    new Option("H", true, "set function depth level"),
+    new Option("k", true, "set kill list"),
+    new Option("z", true, "set cpu affinity list"),
+    new Option("Y", true, "set sched priority list"),
+    new Option("v", false, "verbose")
   ]),
-  new Command("disktop", []),
-  new Command("nettop", []),
-  new Command("filetop", []),
-  new Command("systop", []),
-  new Command("usertop", [])
+  new Command("disktop", [
+    new Option("e", true, "enable options\n" +
+                          "                a:affinity | b:block | c:cpu | C:cgroup\n" +
+                          "                d:disk | D:DLT | e:encode | E:Elastic\n" +
+                          "                f:float | F:wfc | h:sigHandler | i:irq\n" +
+                          "                L:cmdline | m:memory | n:net | o:oomScore\n" +
+                          "                p:pipe | P:perf | r:report | R:fileReport\n" +
+                          "                s:stack | S:pss | t:thread | u:uss\n" +
+                          "                w:wss | W:wchan"),
+    new Option("d", true, "disable options\n" +
+                          "                a:memAvailable | A:cpuAverage\n" +
+                          "                c:cpu | e:encode | G:gpu | L:log\n" +
+                          "                p:print | t:truncate | T:task"),
+    new Option("o", true, "save output data"),
+    new Option("u", false, "run in the background"),
+    new Option("W", false, "wait for signal"),
+    new Option("b", true, "set buffer size"),
+    new Option("T", true, "set font path"),
+    new Option("j", true, "set report path"),
+    new Option("w", true, "set additional command"),
+    new Option("x", true, "set local address"),
+    new Option("X", true, "set request address"),
+    new Option("N", true, "set report address"),
+    new Option("S", true, "sort by key"),
+    new Option("P", false, "group threads in same process"),
+    new Option("I", true, "set input path"),
+    new Option("m", true, "set terminal size"),
+    new Option("a", false, "show all stats and events"),
+    new Option("g", true, "set filter"),
+    new Option("i", true, "set interval"),
+    new Option("R", true, "set repeat count"),
+    new Option("Q", false, "print all rows in a stream"),
+    new Option("E", true, "set cache dir path"),
+    new Option("H", true, "set function depth level"),
+    new Option("k", true, "set kill list"),
+    new Option("z", true, "set cpu affinity list"),
+    new Option("Y", true, "set sched priority list"),
+    new Option("v", false, "verbose")
+  ]),
+  new Command("nettop", [
+    new Option("e", true, "enable options\n" +
+                          "                a:affinity | b:block | c:cpu | C:cgroup\n" +
+                          "                d:disk | D:DLT | e:encode | E:Elastic\n" +
+                          "                f:float | F:wfc | h:sigHandler | i:irq\n" +
+                          "                L:cmdline | m:memory | n:net | o:oomScore\n" +
+                          "                p:pipe | P:perf | r:report | R:fileReport\n" +
+                          "                s:stack | S:pss | t:thread | u:uss\n" +
+                          "                w:wss | W:wchan"),
+    new Option("d", true, "disable options\n" +
+                          "                a:memAvailable | A:cpuAverage\n" +
+                          "                c:cpu | e:encode | G:gpu | L:log\n" +
+                          "                p:print | t:truncate | T:task"),
+    new Option("o", true, "save output data"),
+    new Option("u", false, "run in the background"),
+    new Option("W", false, "wait for signal"),
+    new Option("b", true, "set buffer size"),
+    new Option("T", true, "set font path"),
+    new Option("j", true, "set report path"),
+    new Option("w", true, "set additional command"),
+    new Option("x", true, "set local address"),
+    new Option("X", true, "set request address"),
+    new Option("N", true, "set report address"),
+    new Option("S", true, "sort by key"),
+    new Option("P", false, "group threads in same process"),
+    new Option("I", true, "set input path"),
+    new Option("m", true, "set terminal size"),
+    new Option("a", false, "show all stats and events"),
+    new Option("g", true, "set filter"),
+    new Option("i", true, "set interval"),
+    new Option("R", true, "set repeat count"),
+    new Option("Q", false, "print all rows in a stream"),
+    new Option("E", true, "set cache dir path"),
+    new Option("H", true, "set function depth level"),
+    new Option("k", true, "set kill list"),
+    new Option("z", true, "set cpu affinity list"),
+    new Option("Y", true, "set sched priority list"),
+    new Option("v", false, "verbose")
+  ]),
+  new Command("filetop", [
+    new Option("e", true, "enable options\n" +
+                          "                p:pipe | e:encode"),
+    new Option("d", true, "disable options\n" +
+                          "                e:encode | p:print"),
+    new Option("o", true, "save output data"),
+    new Option("u", false, "run in the background"),
+    new Option("W", false, "wait for signal"),
+    new Option("b", true, "set buffer size"),
+    new Option("w", true, "set additional command"),
+    new Option("x", true, "set local address"),
+    new Option("X", true, "set request address"),
+    new Option("N", true, "set report address"),
+    new Option("S", true, "sort by key"),
+    new Option("m", true, "set terminal size"),
+    new Option("a", false, "show all stats and events"),
+    new Option("g", true, "set filter"),
+    new Option("i", true, "set interval"),
+    new Option("R", true, "set repeat count"),
+    new Option("Q", false, "print all rows in a stream"),
+    new Option("E", true, "set cache dir path"),
+    new Option("k", true, "set kill list"),
+    new Option("z", true, "set cpu affinity list"),
+    new Option("Y", true, "set sched priority list"),
+    new Option("v", false, "verbose")
+  ]),
+  new Command("systop", [
+    new Option("e", true, "enable options\n" +
+                          "                a:affinity | b:block | c:cpu | C:cgroup\n" +
+                          "                d:disk | D:DLT | e:encode | E:Elastic\n" +
+                          "                f:float | F:wfc | h:sigHandler | i:irq\n" +
+                          "                L:cmdline | m:memory | n:net | o:oomScore\n" +
+                          "                p:pipe | P:perf | r:report | R:fileReport\n" +
+                          "                s:stack | S:pss | t:thread | u:uss\n" +
+                          "                w:wss | W:wchan"),
+    new Option("d", true, "disable options\n" +
+                          "                a:memAvailable | A:cpuAverage\n" +
+                          "                c:cpu | e:encode | G:gpu | L:log\n" +
+                          "                p:print | t:truncate | T:task"),
+    new Option("o", true, "save output data"),
+    new Option("u", false, "run in the background"),
+    new Option("W", false, "wait for signal"),
+    new Option("b", true, "set buffer size"),
+    new Option("T", true, "set font path"),
+    new Option("j", true, "set report path"),
+    new Option("w", true, "set additional command"),
+    new Option("x", true, "set local address"),
+    new Option("X", true, "set request address"),
+    new Option("N", true, "set report address"),
+    new Option("S", true, "sort by key"),
+    new Option("P", false, "group threads in same process"),
+    new Option("I", true, "set input path"),
+    new Option("m", true, "set terminal size"),
+    new Option("a", false, "show all stats and events"),
+    new Option("g", true, "set filter"),
+    new Option("i", true, "set interval"),
+    new Option("R", true, "set repeat count"),
+    new Option("Q", false, "print all rows in a stream"),
+    new Option("E", true, "set cache dir path"),
+    new Option("H", true, "set function depth level"),
+    new Option("k", true, "set kill list"),
+    new Option("z", true, "set cpu affinity list"),
+    new Option("Y", true, "set sched priority list"),
+    new Option("v", false, "verbose")
+  ]),
+  new Command("usertop", [
+    new Option("e", true, "enable options\n" +
+                          "                a:affinity | b:block | c:cpu | C:cgroup\n" +
+                          "                d:disk | D:DLT | e:encode | E:Elastic\n" +
+                          "                f:float | F:wfc | h:sigHandler | i:irq\n" +
+                          "                L:cmdline | m:memory | n:net | o:oomScore\n" +
+                          "                p:pipe | P:perf | r:report | R:fileReport\n" +
+                          "                s:stack | S:pss | t:thread | u:uss\n" +
+                          "                w:wss | W:wchan"),
+    new Option("d", true, "disable options\n" +
+                          "                a:memAvailable | A:cpuAverage\n" +
+                          "                c:cpu | e:encode | G:gpu | L:log\n" +
+                          "                p:print | t:truncate | T:task"),
+    new Option("o", true, "save output data"),
+    new Option("u", false, "run in the background"),
+    new Option("W", false, "wait for signal"),
+    new Option("b", true, "set buffer size"),
+    new Option("T", true, "set font path"),
+    new Option("j", true, "set report path"),
+    new Option("w", true, "set additional command"),
+    new Option("x", true, "set local address"),
+    new Option("X", true, "set request address"),
+    new Option("N", true, "set report address"),
+    new Option("S", true, "sort by key"),
+    new Option("P", false, "group threads in same process"),
+    new Option("I", true, "set input path"),
+    new Option("m", true, "set terminal size"),
+    new Option("a", false, "show all stats and events"),
+    new Option("g", true, "set filter"),
+    new Option("i", true, "set interval"),
+    new Option("R", true, "set repeat count"),
+    new Option("Q", false, "print all rows in a stream"),
+    new Option("E", true, "set cache dir path"),
+    new Option("H", true, "set function depth level"),
+    new Option("k", true, "set kill list"),
+    new Option("z", true, "set cpu affinity list"),
+    new Option("Y", true, "set sched priority list"),
+    new Option("v", false, "verbose")
+  ])
 ];
