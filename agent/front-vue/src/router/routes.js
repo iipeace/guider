810a87d7c89e475eec270fde7212afbe3ef51b17
agrajak <korbots@gmail.com>
2019-10-17 21:26:11 +0900

    Add DateTimePicker(vue-datetime) in History.js Change 'Background.vue' to 'History.vue'

--- agent/front-vue/src/router/routes.js
+++ agent/front-vue/src/router/routes.js
@@ -6,13 +6,13 @@ const Dashboard = () =>
   import(/* webpackChunkName: "dashboard" */ "@/pages/Dashboard.vue");
 const Command = () =>
   import(/* webpackChunkName: "dashboard" */ "@/pages/Command.vue");
-const Background = () =>
-  import(/* webpackChunkName: "dashboard" */ "@/pages/Background.vue");
+const History = () =>
+  import(/* webpackChunkName: "dashboard" */ "@/pages/History.vue");
 
 const routes = [
   { path: "/dashboard", name: "Dashboard", alias: "/", component: Dashboard },
   { path: "/command", name: "Command", component: Command },
-  { path: "/background", name: "Background", component: Background },
+  { path: "/history", name: "History", component: History },
   { path: "*", component: NotFound }
 ];
 
