9630f8c33727f43d56dd7dcb9a9c7d1c5f486888
agrajak <korbots@gmail.com>
2019-10-18 21:26:06 +0900

    Enable 'end' range at History.vue Show how many databases are stored in mongodb.
    Fix lint issues

--- agent/front-vue/src/pages/History.vue
+++ agent/front-vue/src/pages/History.vue
@@ -3,7 +3,18 @@
     <b-row>
       <b-col sm="12">
         <b-card style="background: white; ">
-          Browse the stored database
+          <b-card v-if="count != -1">
+            <b-card-text v-if="count != 0">
+              There are {{ count }} pieces of information from {{ db_start }} to
+              {{ db_end }}.
+            </b-card-text>
+            <b-card-text v-else>
+              There are 0 pieces of information.
+            </b-card-text>
+          </b-card>
+          <h3 style="color: black;">
+            Browse the stored database.
+          </h3>
           <b-row>
             <b-col sm="6">
               From <br />
@@ -11,14 +22,15 @@
             </b-col>
             <b-col sm="6">
               To <br />
-              <datetime type="datetime" :value="now" disabled></datetime>
+              <datetime type="datetime" v-model="end"></datetime>
             </b-col>
             <b-col sm="6">
               Sample # <br />
               <input v-model="sampleNum" />
             </b-col>
           </b-row>
-          <input type="button" value="Browse!" @click="getData" /> {{ errMsg }}
+          <b-button type="button" @click="getData">Browse</b-button>
+          {{ errMsg }}
         </b-card>
       </b-col>
       <b-col sm="6">
@@ -76,50 +88,82 @@ export default {
       dataSet: new GuiderGraphDataSet(),
       start: null,
       sampleNum: 40,
-      errMsg: ""
+      errMsg: "",
+      count: -1,
+      db_start: null,
+      db_end: null
     };
   },
-  computed: {
-    now: function() {
-      return new Date().toISOString();
-    }
-  },
   methods: {
+    tsToPrettyDate(timestamp) {
+      return new Date(timestamp * 1000).toLocaleString();
+    },
+    tsToISOString(timestamp) {
+      return new Date(timestamp * 1000).toISOString();
+    },
+    dateToTS(date) {
+      return Date.parse(date) / 1000;
+    },
+    async getMetaData() {
+      const response = await fetch("http://localhost:8000/dataset");
+      const responseOK = response && response.status == 200;
+      if (responseOK) {
+        const body = await response.json();
+        if (body && body.status == "ok") {
+          const { data, meta } = body;
+          if (data.length) {
+            this.count = meta.count || 0;
+            this.db_start = this.tsToPrettyDate(meta.start);
+            this.start = this.tsToISOString(meta.start);
+            this.db_end = this.tsToPrettyDate(meta.end);
+            this.end = this.tsToISOString(meta.end);
+          } else {
+            this.count = 0;
+          }
+        }
+      }
+    },
     async getData() {
       // init values
       this.errMsg = "";
       this.dataSet = new GuiderGraphDataSet();
-      try {
-        let url = "http://localhost:8000/dataset";
-        let hasQuery = false;
-        if (this.start) {
-          let timestamp = Date.parse(this.start) / 1000;
-          url += `?start=${timestamp}`;
-          hasQuery = true;
-        }
-        if (this.sampleNum >= 20 && this.sampleNum <= 100) {
-          url += (hasQuery ? "&" : "?") + `num=${this.sampleNum}`;
-        } else throw Error("Sample Number should be in range 20~100");
-        const response = await fetch(url);
-        const responseOK = response && response.status == 200;
-        if (responseOK) {
-          const body = await response.json();
-          if (body && body.status == "ok") {
-            const { data } = body;
-            data
-              .map(x => Object({ timestamp: x.timestamp, ...x.data }))
-              .forEach(x => {
-                this.dataSet.setGuiderData(x);
-              });
-          } else throw Error("Internal Server Error");
-        } else throw Error("Failed to load body");
-      } catch (e) {
-        this.errMsg = e;
+      let url = "http://localhost:8000/dataset";
+      let hasQuery = false;
+      if (this.start) {
+        let timestamp = this.dateToTS(this.start);
+        url += `?start=${timestamp}`;
+        hasQuery = true;
       }
+      if (this.end) {
+        let timestamp = this.dateToTS(this.end);
+        url += (hasQuery ? "&" : "?") + `end=${timestamp}`;
+        hasQuery = true;
+      }
+      if (this.sampleNum >= 20 && this.sampleNum <= 100) {
+        url += (hasQuery ? "&" : "?") + `num=${this.sampleNum}`;
+      } else throw Error("Sample Number should be in range 20~100");
+      const response = await fetch(url);
+      const responseOK = response && response.status == 200;
+      if (responseOK) {
+        const body = await response.json();
+        if (body && body.status == "ok") {
+          const { data } = body;
+          data
+            .map(x => Object({ timestamp: x.timestamp, ...x.data }))
+            .forEach(x => {
+              this.dataSet.setGuiderData(x);
+            });
+        } else throw Error("Internal Server Error");
+      } else throw Error("Failed to load body");
     }
   },
   async mounted() {
-    await this.getData();
+    try {
+      await this.getMetaData();
+      await this.getData();
+    } catch (e) {
+      this.errMsg = e;
+    }
   },
   beforeDestroy() {}
 };
