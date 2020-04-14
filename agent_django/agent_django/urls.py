d7b1c81cd6a452f2419b89a6d887b38629a03ea4
yoonje <yoonjechoi.tech@gmail.com>
2020-04-14 22:19:47 +0900

    add return index.js functions

--- agent_django/agent_django/urls.py
+++ agent_django/agent_django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
 ]
