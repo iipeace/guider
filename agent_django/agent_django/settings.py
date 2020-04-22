fe362323ae6840fd064c4db7070dad02252ad4eb
yoonje <yoonjechoi.tech@gmail.com>
2020-04-23 04:54:13 +0900

    fix allow host to wildcard

--- agent_django/agent_django/settings.py
+++ agent_django/agent_django/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
