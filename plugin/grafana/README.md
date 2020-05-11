ffb0119330943f951cf2d177957e7891f8dd1841
yoonje <elxm6123@gmail.com>
2020-05-11 17:59:44 +0900

    add base network code in grafana

--- plugin/grafana/README.md
+++ plugin/grafana/README.md
@@ -1,10 +1,12 @@
-# System Status Visualization by grafana #
+# Guider Grafana
 
-1. Upload stats from guider.report to influxDB
-    # cd guider/plugin/grafana/
-    # guider reptop -j . -u
-    # ./visualization.py
+### Requirement
+```
+- Grafana
+- InfluxDB
+```
 
-2. Check grafana web page
-    - grafana prototype page : http://13.124.47.121:3000  
-        -> request ID/PW to pyotel@gmail.com  
+### Run
+```sh
+$ python3 visualization.py {Guider Server IP}
+```
---
+++ plugin/grafana/visualize.py
@@ -0,0 +1,42 @@
+import json
+import sys
+
+from common.guider import GuiderInstance, RequestManager
+
+
+def get_data_by_command(target_addr, request_id, cmd):
+    result = dict(result=0, data=dict(), errMsg='')
+    if cmd is None or cmd is '' or target_addr is None or target_addr is '':
+        result['result'] = -1
+        result['errorMsg'] = 'Required data missing'
+        sys.exit(0)
+    try:
+        RequestManager.add_request(request_id)
+        GuiderInstance.set_network_manager(target_addr)
+        pipe = GuiderInstance.get_command_pipe(target_addr, cmd)
+        while RequestManager.get_request_status(request_id):
+            str_pipe = pipe.getData()
+            if not str_pipe:
+                break
+            result['data'] = str_pipe.replace('\n', '</br>')
+        print(result)
+        pipe.close()
+        stop_command_run(request_id)
+    except Exception as e:
+        print(e)
+        if pipe:
+            pipe.close()
+        stop_command_run(request_id)
+
+
+def stop_command_run(request_id):
+    result = dict(result=0, data='stop success', errMsg='')
+    if RequestManager.get_request_status(request_id):
+        RequestManager.stop_request(request_id)
+    else:
+        result['result'] = -1
+        result['errMsg'] = 'stop failed'
+
+
+if __name__ == '__main__':
+    get_data_by_command(1, "GUIDER reptop -J")
