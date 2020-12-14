978e639c83762c348c1c2814d6c3190c41428eff
Peace Lee <iipeace5@gmail.com>
2020-12-14 23:47:07 +0900

    timeline: Add skeleton code for timeline graph
    Signed-off-by: Peace Lee <iipeace5@gmail.com>

--- guider/guider.py
+++ guider/guider.py
@@ -7,7 +7,7 @@ __module__ = "guider"
 __credits__ = "Peace Lee"
 __license__ = "GPLv2"
 __version__ = "3.9.7"
-__revision__ = "201213"
+__revision__ = "201214"
 __maintainer__ = "Peace Lee"
 __email__ = "iipeace5@gmail.com"
 __repository__ = "https://github.com/iipeace/guider"
@@ -6600,6 +6600,196 @@ class NetworkMgr(object):
             self.close()
         except:
             pass
+
+
+
+
+
+class Timeline(object):
+    class Segment(object):
+        def __init__(self, group, time_start, time_end, extra):
+            self.group = group
+            self.time_start = time_start
+            self.time_end = time_end
+            self.text = None
+            self._init_extra(extra)
+
+        def _init_extra(self, extra):
+            if "text" in extra:
+                self.text = extra["text"]
+
+
+
+    class Config(object):
+        def __init__(self):
+            # get svgwrite object #
+            svgwrite = SysMgr.getPkg('svgwrite')
+
+            self.WIDTH = 120000
+            self.HEIGHT = 40000
+            self.TIME_AXIS_HEIGHT = 5
+            self.TICKS = 3
+            self.PALETTE = [svgwrite.rgb(244, 67, 54)]
+            self.FONT_SIZE = 0.1
+            self.LABEL_SIZE_MIN = 0.1
+
+        @staticmethod
+        def load(file_name):
+            with open(file_name) as json_file:
+                # get svgwrite, json object #
+                svgwrite = SysMgr.getPkg('svgwrite')
+                json = SysMgr.getPkg('json')
+
+                data = json.load(json_file)
+                config = Timeline.Config()
+                config.WIDTH = data.get("width", 800)
+                config.HEIGHT = data.get("height", 200)
+                config.FONT_SIZE = data.get("font_size", 2)
+                config.TICKS = data.get("time_ticks", 2)
+                config.TIME_AXIS_HEIGHT = data.get("time_axis_height", 10)
+                config.LABEL_SIZE_MIN = data.get("label_size_min", 10)
+                palette = []
+                for palette_entry in data.get("palette", []):
+                    rgb = [int(rgb_value) for rgb_value in re.findall("\d+", palette_entry)]
+                    palette.append(svgwrite.rgb(rgb[0], rgb[1], rgb[2]))
+                config.PALETTE = palette
+                return config
+
+
+
+    def __init__(self, segments, time_unit, config):
+        self.segments = segments
+        self.time_unit = time_unit
+        self.config = config
+        self.time_start = \
+            min(segments, key=lambda segment: segment.time_start).time_start
+        self.time_end = \
+            max(segments, key=lambda segment: segment.time_end).time_end
+        self.groups = len(set(s.group for s in self.segments))  # todo case groups empty
+        self.scaled_height = self.config.HEIGHT / self.groups
+        self.ratio = self.config.WIDTH / (self.time_end - self.time_start)
+        self.color_map = self._build_color_map()
+
+
+
+    def _build_color_map(self):
+        color_map = {}
+        segment_groups = set(s.group for s in self.segments)
+        for i, group in enumerate(segment_groups):
+            color_map[group] = self.config.PALETTE[i % len(self.config.PALETTE)]
+        return color_map
+
+
+
+    def _draw_grid(self, dwg):
+        for x in range(0, self.config.WIDTH, self.config.TICKS):
+            dwg.add(dwg.line(
+                (x, 0), (x, self.config.HEIGHT),
+                stroke='black', stroke_width=0.1))
+
+
+
+    def _draw_time_axis(self, dwg):
+        dwg.add(dwg.rect(
+            (0, self.config.HEIGHT),
+            (self.config.WIDTH, self.config.TIME_AXIS_HEIGHT), fill='white'))
+
+        y_time_tick = self.config.HEIGHT + self.config.TIME_AXIS_HEIGHT / 2
+
+        for x_tick_time in range(0, self.config.WIDTH, self.config.TICKS):
+            tick_time = "{:10.2f}".format(x_tick_time * (1 / self.ratio))
+            dwg.add(dwg.text(tick_time,
+                (x_tick_time, y_time_tick), font_size=self.config.FONT_SIZE))
+
+
+
+    def _draw_background(self, dwg):
+        dwg.add(dwg.rect((0, 0),
+            (self.config.WIDTH, self.config.HEIGHT), fill='white'))
+
+
+
+    def _draw_segments(self, dwg):
+        for segment in self.segments:
+            self._draw_segment(segment, dwg)
+
+
+
+    def _draw_segment(self, segment, dwg):
+        x0 = (segment.time_start - self.time_start) * self.ratio
+        x1 = (segment.time_end - self.time_start) * self.ratio
+        y0 = self.scaled_height * (segment.group % self.groups)
+        scaled_width = (x1 - x0)
+        color = self.color_map[segment.group]
+
+        dwg.add(dwg.rect((x0, y0),
+            (scaled_width, self.scaled_height),
+            rx=1, ry=1, fill=color, fill_opacity=0.5))
+
+        if scaled_width < self.config.LABEL_SIZE_MIN:
+            return
+
+        segment_label = "{} ({} {})".format(
+            segment.text, segment.time_end - segment.time_start, self.time_unit)
+
+        dwg.add(dwg.text(segment_label,
+            (x0, y0 + self.scaled_height * 0.25),
+            font_size=self.config.FONT_SIZE))
+
+        time_start_label = "{} {}".format(
+            segment.time_start - self.time_start, self.time_unit)
+
+        time_end_label = "{} {}".format(
+            segment.time_end - self.time_start, self.time_unit)
+
+        dwg.add(dwg.text(
+            time_start_label,
+            (x0, y0 + self.scaled_height * 0.5),
+            font_size=self.config.FONT_SIZE))
+
+        dwg.add(dwg.text(time_end_label,
+            (x0, y0 + self.scaled_height * 0.75),
+            font_size=self.config.FONT_SIZE))
+
+
+
+    def draw(self, dwg):
+        self._draw_background(dwg)
+        self._draw_grid(dwg)
+        self._draw_segments(dwg)
+        self._draw_time_axis(dwg)
+
+
+
+    @staticmethod
+    def load(file_name, config):
+        with open(file_name) as json_file:
+            # get json object #
+            json = SysMgr.getPkg('json')
+
+            data = json.load(json_file)
+            segments = Timeline._load_segments(data)
+            time_unit = "???"
+            if "time_unit" in data:
+                time_unit = data["time_unit"]
+            return Timeline(segments, time_unit, config)
+
+
+
+    @staticmethod
+    def _load_segments(data):
+        segments = []
+        for segment_data in data["segments"]:
+            extra = {}
+            if "text" in segment_data:
+                extra["text"] = segment_data["text"]
+            assert segment_data["time_start"] < segment_data["time_end"]
+
+            segments.append(Timeline.Segment(
+                segment_data["group"], segment_data["time_start"],
+                segment_data["time_end"], extra))
+
+        return segments
 
 
 
@@ -14843,6 +15033,7 @@ class SysMgr(object):
     rankProcEnable = True
     inotifyEnable = False
     dwarfEnable = False
+    barGraphEnable = False
 
     # Elastic Stack #
     elasticEnable = False
@@ -17559,6 +17750,9 @@ Options:
 Examples:
     - Monitor status of {2:2} used CPU resource more than 1%%
         # {0:1} {1:1}
+
+    - Monitor status of all {2:2} with bar graphs for cores
+        # {0:1} {1:1} -a -e B
 
     - Monitor status of all {2:2} sorted by memory(RSS)
         # {0:1} {1:1} -S m
@@ -22372,6 +22566,11 @@ Copyright:
                 else:
                     disableStat += 'REPORT '
 
+                if SysMgr.barGraphEnable:
+                    enableStat += 'BAR '
+                else:
+                    disableStat += 'BAR '
+
         elif SysMgr.isFunctionMode():
             SysMgr.printInfo("<FUNCTION MODE>")
 
@@ -23642,6 +23841,21 @@ Copyright:
             "fail to import python package: %s, "
             "try to enter 'pip%s install %s'") % \
                 (name, sys.version_info[0], pkg), True)
+
+
+
+    @staticmethod
+    def drawTimeline(inputPath, outputPath, configPath=None):
+        # get svgwrite object #
+        svgwrite = SysMgr.getPkg('svgwrite')
+
+        dwg = svgwrite.Drawing(outputPath, profile='tiny')
+        config = Timeline.Config()
+        if configPath:
+            config = Timeline.Config.load(configPath)
+        timeline = Timeline.load(inputPath, config)
+        timeline.draw(dwg)
+        dwg.save()
 
 
 
@@ -24970,6 +25184,9 @@ Copyright:
                         SysMgr.blockEnable = True
                     else:
                         sys.exit(0)
+
+                if 'B' in options:
+                    SysMgr.barGraphEnable = True
 
                 if 's' in options:
                     if SysMgr.checkStackTopCond():
@@ -30736,7 +30953,7 @@ Copyright:
                         sym, size = binObj.getSymbolByOffset(
                             addr, onlyFunc=False)
                         resInfo[addr] = [sym, filePath, 'N/A']
-                        if len(sym) > maxSymLen:
+                        if maxSymLen < len(sym) < SysMgr.ttyCols/2:
                             maxSymLen = len(sym)
                     except SystemExit:
                         sys.exit(0)
@@ -30773,7 +30990,7 @@ Copyright:
                     sys.exit(0)
                 elif type(ret) is list:
                     resInfo[addr] = [ret[0], ret[1], ret[2]]
-                    if len(ret[0]) > maxSymLen:
+                    if maxSymLen < len(ret[0]) < SysMgr.ttyCols/2:
                         maxSymLen = len(ret[0])
                 else:
                     resInfo[addr] = ['??', '??', 'N/A']
@@ -31142,8 +31359,8 @@ Copyright:
                         else:
                             addr = hex(foffset)
 
-                        resInfo['%s|%s' % (item[1], filePath)] = \
-                            (hex(item[0]), filePath, addr, item[1])
+                        resInfo['%s|%s' % (item[1], origFilePath)] = \
+                            (hex(item[0]), origFilePath, addr, item[1])
                 except SystemExit:
                     sys.exit(0)
                 except:
@@ -31201,7 +31418,7 @@ Copyright:
                             resInfo['%s|%s' % (item[1], filePath)] = \
                                 (hex(item[0]), filePath, None, item[1])
 
-                            if len(item[1]) > maxSymLen:
+                            if maxSymLen < len(item[1]) < SysMgr.ttyCols/2:
                                 maxSymLen = len(item[1])
                     except SystemExit:
                         sys.exit(0)
@@ -31229,7 +31446,7 @@ Copyright:
             # get symbol offset #
             resInfo = SysMgr.getProcAddrBySymbol(pid, symbolList)
             for key, item in resInfo.items():
-                if len(item[3]) > maxSymLen:
+                if maxSymLen < len(item[3]) < SysMgr.ttyCols/2:
                     maxSymLen = len(item[3])
 
         if procInfo:
@@ -69759,7 +69976,7 @@ class ThreadAnalyzer(object):
                         lenCoreStat = UtilMgr.getRealLen(coreStat)
 
                     # use short core stats for many-core system #
-                    if SysMgr.nrCore > 8:
+                    if not SysMgr.barGraphEnable and SysMgr.nrCore > 8:
                         shortCoreStats += coreStat
                         coreFactor = long(SysMgr.ttyCols / lenCoreStat)
                         if (curCore+1) % coreFactor == 0:
