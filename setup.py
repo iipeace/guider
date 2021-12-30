9a55f5aea9245cef09de9d56af31ac1050117c75
Peace Lee <iipeace5@gmail.com>
2021-12-30 15:03:31 +0900

    setup: Update version to 3.9.89
    Signed-off-by: Peace Lee <iipeace5@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import site, os, sys, shutil
 
 setup(
     name = 'guider',
-    version = '3.9.88',
+    version = '3.9.89',
     license = 'GPL2',
     description = 'A system-wide analyzer of performance',
     author = 'Peace Lee',
